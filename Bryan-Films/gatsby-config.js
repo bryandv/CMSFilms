0d50259f23580bb611515ab1758ce492e208859e
Bryan Devolder <bryan_dv@hotmail.com>
2020-12-14 15:37:43 +0100

    setting up link with wordpress

--- Bryan-Films/gatsby-config.js
+++ Bryan-Films/gatsby-config.js
@@ -26,7 +26,28 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      
     },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "WPGraphql",
+        fieldName: "wpcontent",
+        url: "http://bryan-films.local/graphql",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      
+      options: {
+      
+      fonts: [`Roboto`, `Oswald`],
+      
+      display: "swap",
+      
+        },
+      }, 
+      `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
